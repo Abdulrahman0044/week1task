10b479832900f0dac5a8c211bfc29d53a2fe0ceb
Abdulrahman Babatunde <abdulrahamanbabatunde12@gmail.com>
2023-03-12 08:23:50 +0100

    Added comments

--- week1Ass.php
+++ week1Ass.php
@@ -1,4 +1,8 @@
 <?php
-    $name = "Abdulrahman Babatunde";
-    echo ($name);
+/*This code print my name
+as the writer of the code
+*/
+    $name = "Abdulrahman Babatunde"; // Declare a variable and store the name
+    #Print out the value store in the location: $name
+    echo $name;
 ?>
